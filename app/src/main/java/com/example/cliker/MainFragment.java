32db09178a956a014cae86e7ca46f9fbad8c4a51
Vadim Guryanov <gvadimmonstors@mail.ru>
2019-04-13 19:51:56 +0300

    I add MainFragment I add version1.0 EasyPoint (Event)

--- app/src/main/java/com/example/cliker/MainFragment.java
+++ app/src/main/java/com/example/cliker/MainFragment.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +23,10 @@ public class MainFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_main, container, false);
         return view;
+    }
+
+    public void onClickEasyPoint() {
+        Intent intent = new Intent();
     }
 
 }
