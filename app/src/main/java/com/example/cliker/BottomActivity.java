9ec70f18c81d9b1512a5e1369e9813308b6905d9
Vadim Guryanov <gvadimmonstors@mail.ru>
2019-04-20 14:26:49 +0300

    I add xml

--- app/src/main/java/com/example/cliker/BottomActivity.java
+++ app/src/main/java/com/example/cliker/BottomActivity.java
@@ -40,8 +40,6 @@ public class BottomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom);
-
-
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_notifications);
