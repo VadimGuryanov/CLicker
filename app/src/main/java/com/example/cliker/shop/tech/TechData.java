5e65fa92016f59721d2382a46073cce0a67e480d
Sergey <seregaryz@gmail.com>
2019-04-24 13:47:11 +0300

    add commit for merging

---
+++ app/src/main/java/com/example/cliker/shop/tech/TechData.java
@@ -0,0 +1,29 @@
+package com.example.cliker.shop.tech;
+
+import com.example.cliker.R;
+
+public class TechData {
+    public static final int CAPASITY = 3;
+
+    public static String[] tech_name = new String[] {
+            "Ноутбук",
+            "Мышка",
+            "Смартфон"
+    };
+
+    public static String[] description = new String[] {
+            "Запрогай что-нибуь на нем",
+            "Тачпад для слабаков",
+            "Тестируй на нем свои андроид приложения"
+    };
+
+    public static int[] point = new int[] {
+            100, 200, 300
+    };
+
+    public static int[] image = new int[] {
+            R.drawable.t_itis,
+            R.drawable.t_rainbow,
+            R.drawable.t_polo
+    };
+}
