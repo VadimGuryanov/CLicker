038bfdd209ff3b980de5816a0ce6ba260cc13f95
Vadim Guryanov <gvadimmonstors@mail.ru>
2019-04-26 10:51:30 +0300

    push

---
+++ app/src/main/java/com/example/cliker/shop/boost/BoostProcessing.java
@@ -0,0 +1,44 @@
+package com.example.cliker.shop.boost;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.io.IOException;
+
+public class BoostProcessing {
+
+    Context context;
+
+    public String myMoney = "myMoney";
+
+    public static final String ALGEM = "boost";
+    public static final String nameKey = "algem_boost";
+
+    private SharedPreferences sharedPrefs;
+
+    public BoostProcessing(Context context) throws IOException {
+        this.context = context;
+    }
+
+
+    // метод для сохранения текста в файл настроек
+    public void saveText(String value) {
+        // получаем текст
+//        String editTextValue = editText.getText().toString();
+
+        // получаем доступ к файлу
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        // сохраняем по текст из EditText по ключу nameKey
+        editor.putString(nameKey, value + " ");
+        editor.apply();
+    }
+
+    // метод для получения текста из SharedPreferences по ключу
+    public String getText() {
+        sharedPrefs = context.getSharedPreferences(ALGEM, Context.MODE_PRIVATE);
+        if (sharedPrefs.contains(nameKey)) {
+            return (sharedPrefs.getString(nameKey, ""));
+        } return "0";
+    }
+
+}
